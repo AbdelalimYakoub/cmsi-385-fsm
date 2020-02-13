2425203642ca95ddde2ebc13e542af6db82eeada
EpsilonNu <36215445+EpsilonNu@users.noreply.github.com>
2020-02-12 18:18:07 -0800

    edited src file

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -3,7 +3,9 @@ export default class DeterministicFiniteStateMachine {
   /**
    */
   constructor({ transitions, startState, acceptStates }) {
-    throw 'IMPLEMENT constructor(description)';
+    this._transitions = transitions;
+    this._startState  = startState;
+    this._acceptStates = acceptStates;
   }
 
   /**
@@ -11,11 +13,23 @@ export default class DeterministicFiniteStateMachine {
    * @returns a string state name
    */
   transition(state, symbol) {
-    throw 'IMPLEMENT transition(state, symbol)';
+    return this._transitions[state][symbol];
   }
 
   accepts(string, state = this._startState) {
-    throw 'IMPLEMENT FiniteStateMachine.accepts';
+    var i;
+    for (i = 0; i < string.length; i++) {
+      nextState = transition(state, string..charAt(i));
+      state = nextState;
+      }
+
+    if(this._acceptStates.includes(state)) {
+      return "Accepted";
+      }
+      else {
+        return "Rejected";
+      }
+    }
   }
 
 }

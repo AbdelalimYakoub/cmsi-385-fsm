e9fc140b92fc473cfed70405b309968e515228cc
EpsilonNu <36215445+EpsilonNu@users.noreply.github.com>
2020-03-04 04:26:44 -0800

    updated minimize fucntion (not done yet)

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -24,6 +24,10 @@ export default class DeterministicFiniteStateMachine {
     return new Set(Object.keys(this.transitions));
   }
 
+  getAcceptStates(){
+    return new Set(this.acceptStates);
+  }
+
   stateAccepted(state) {
     return this.acceptStates.includes(state);
   }
@@ -36,7 +40,7 @@ export default class DeterministicFiniteStateMachine {
     if(!this.transitions[state]) return;
     return this.transitions[state][symbol];
   }
-
+  
   accepts(string, state = this.startState) {
     const nextState = this.transition(state, string.charAt(0));
     return (string.length === 0) ? this.stateAccepted(state) :
@@ -101,7 +105,111 @@ export function minus(dfa1, dfa2) {
     (state1, state2) => dfa1.stateAccepted(state1) && !dfa2.stateAccepted(state2));
 }
 
+function getStateSetMap(states, sets) {
+  var index = 0;
+  var stateSetMap = {};
+
+  for (const set of Object.values(sets)) {
+    for (const state of set) {
+      stateSetMap[state] = index;
+    }
+    index++;
+  }
+  return stateSetMap;
+}
+
+function getStateInputSetMap(dfa, minimizedSets) {
+  const stateInputSetMap = {}
+  for (const key of Object.keys(dfa.transitions)) {
+    stateInputSetMap[key] = {...dfa.transitions[key]};
+  }
+  const stateSetMap = getStateSetMap([,,,dfa.states()], minimizedSets);
+
+  for (const key0 of  Object.keys(stateInputSetMap)) {
+    for (const key1 in Object.keys(stateInputSetMap[key0])) {
+      stateInputSetMap[key0][key1] = stateSetMap[stateInputSetMap[key0][key1]];
+    }
+  }
+  return stateInputSetMap;
+}
+
 export function minimize(dfa) {
-  // TODO return a new minimized DFA
+  const acceptStatesSet = dfa.getAcceptStates();
+  
+  const nonAcceptStatesSet = dfa.states();
+  for (const acceptState of acceptStatesSet) {
+    nonAcceptStatesSet.delete(acceptState);
+  }
+
+  var flag = 0;
+  var minimizedSets = {};
+  if(acceptStatesSet.size > 0) {
+    minimizedSets[0] = [...acceptStatesSet];
+    flag = 1;
+  }
+  if(nonAcceptStatesSet.size > 0) {
+    if(flag == 0) {
+      minimizedSets[0] = [...nonAcceptStatesSet];
+    }
+    else {
+      minimizedSets[1] = [...nonAcceptStatesSet];
+    }
+  }
+
+  var previous_size = 0;
+  var current_size = Object.keys(minimizedSets).length;
+ 
+  while(current_size != previous_size) {
+    // Update previous size
+    previous_size = current_size;
+    const stateInputSetMap = getStateInputSetMap(dfa, minimizedSets);
+    var stateNum = Object.keys(stateInputSetMap).length;
+    var mapKeys = [...new Set(Object.keys(stateInputSetMap))];
+    var mapValues = [...new Set(Object.values(stateInputSetMap))];
+    var setIndex = 0;
+ 
+    for(var i = 0; i < stateNum; i++) {
+      if(JSON.stringify(mapValues[i]) !== undefined) {   
+        minimizedSets[setIndex] = [mapKeys[i]];
+        for (var q = i+1; q < stateNum; q++) {
+          if(JSON.stringify(mapValues[q]) !== undefined) {
+            if(JSON.stringify(mapValues[i]) === JSON.stringify(mapValues[q])) {
+              minimizedSets[setIndex].push(mapKeys[q]);
+              delete mapValues[q];
+            }         
+          }
+        }
+        delete mapValues[i];
+        setIndex++;
+      }
+    }
+    current_size = Object.keys(minimizedSets).length;
+  }
+
+  // Constructing new dfa
+  const startState = dfa.startState;
+  const acceptStates = [];
+  const transitions = {};
+  const alphabet = new Set([...dfa.alphabet()]);
+  
+  const stateName = []; 
+  for (const list of Object.values(minimizedSets)) {
+    stateName.push(list[0]);
+  }
+
+  const stateSetMap = getStateSetMap(dfa.states(), minimizedSets);
+
+  for(const state of stateName) {
+    transitions[state] = {};
+    for(const symbol of alphabet) {
+      transitions[state][symbol] = stateName[stateSetMap[dfa.transition(state, symbol)]];
+    }
+  }
+
   return dfa;
+  return new DeterministicFiniteStateMachine({
+    acceptStates,
+    startState,
+    transitions
+  });
 }
